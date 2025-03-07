ff48e04284ef2447309f45e38c515bfd7fbe171b
Joey Gaule <150631386+joeygaule@users.noreply.github.com>
2025-03-07 01:52:36 -0600

    Update username and fix os.join bug

--- app.py
+++ app.py
@@ -2,21 +2,18 @@ import os
 from datetime import datetime
 
 # Please customize the following variables
-USER_NAME = "shahbazc"
+USER_NAME = "jgaule"
 VERSION = "1.0.0"
 
 def say_hi(msg:str = "Hi!", file_directory:str = "/app/data/") -> None:
     # Generate timestamp
     timestamp = datetime.now().strftime("%Y%m%d%H%M")
-
     # Define filename with timestamp
     file_name = f"outputfile_{USER_NAME}_{VERSION}_timestamp_{timestamp}.txt"
-    file_path = os.join(file_directory, file_name)
-
+    file_path = os.path.join(file_directory, file_name)  # Fixed this line
     # Write the timestamp inside the file
     with open(file_path, "w") as file:
         file.write(msg)
-
     print(f"File '{file_path}' created successfully.")
 
 def add_numbers(a:int, b:int) -> int:
